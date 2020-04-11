219beabac6caba8307ee827ce0be1c4e2f11195e
sqlwwx <wwx@wuweixing.com>
2020-04-11 14:38:34 +0800

    feat: clusterPendingLimit

--- sample/consumer.js
+++ sample/consumer.js
@@ -7,33 +7,36 @@ const {
   accessKeyId, accessKeySecret, topic, endpoint, consumerGroup, instanceId
 } = require('./config.js')
 
-let clusterPendingCount = 0
+const clusterPendingCounts = {
+}
 
-const incrementPendingCount = async count => {
-  clusterPendingCount += count
-  return clusterPendingCount
+const incrementPendingCount = async (key, count) => {
+  clusterPendingCounts[key] = clusterPendingCounts[key] || 0
+  clusterPendingCounts[key] += count
+  return clusterPendingCounts[key]
 }
 
 const client = new MQClient(endpoint, accessKeyId, accessKeySecret, null, {
-  pullBatchSize: 2,
+  pullBatchSize: 1,
   pullTimeDelayMillsWhenFlowControl: 3000 / (5 + 3),
   pullThresholdForQueue: 5
 })
 
 let delay = 0
 let count = 0
-const onMsg = uid => async msg => {
-}
 
 const subscribeMsg = consumer => {
   consumer.subscribe(async msg => {
-    const pendingCount = await incrementPendingCount(1)
+    await incrementPendingCount(consumerGroup, 1)
     const body = JSON.parse(msg.body)
     delay += Date.now() - body.timestamp
     count += 1
-    client.logger.info(consumer.nonce, '>>>>>>>', delay, count, delay / count, consumer.pendingCount, pendingCount)
+    client.logger.info(consumer.nonce, '>>>>>>>', delay, count, delay / count, consumer.pendingCount, clusterPendingCounts)
     await sleep(3000)
-    await incrementPendingCount(-1)
+    await incrementPendingCount(consumerGroup, -1)
+  }, {
+    clusterPendingLimit: 8,
+    incrementPendingCount
   })
 }
 
@@ -42,3 +45,7 @@ const subscribeMsg = consumer => {
   client.getConsumer(instanceId, topic, consumerGroup, 'test'),
   client.getConsumer(instanceId, topic, consumerGroup, 'test')
 ].map(subscribeMsg)
+
+setInterval(() => {
+  console.log(count ? delay / count : 0, count, clusterPendingCounts)
+}, 2000)
